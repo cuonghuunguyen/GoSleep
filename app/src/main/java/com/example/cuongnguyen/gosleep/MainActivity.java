4a3cb0bdb9d62d9dbdf9c6565b03eda451312b22
oanhdaoitdnpnv <oanh.dao@student.passerellesnumeriques.org>
2018-12-29 15:02:42 +0700

    done add giao dien

--- app/src/main/java/com/example/cuongnguyen/gosleep/MainActivity.java
+++ app/src/main/java/com/example/cuongnguyen/gosleep/MainActivity.java
@@ -1,13 +1,226 @@
 package com.example.cuongnguyen.gosleep;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtDate,txtTime,txtTimeEnd;
+    EditText editCv,editNd;
+    Button btnDate,btnTime,btnAdd,btnTimeEnd;
+    //Khai báo Datasource lưu trữ danh sách công việc
 
+    ListView lvCv;
+    Calendar cal;
+    Date dateFinish;
+    Date hourFinish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.index);
+        setContentView(R.layout.add_time);
+        getFormWidgets();
+        getDefaultInfor();
+        addEventFormWidgets();
     }
+    /**
+     * hàm dùng để load các control theo Id
+     */
+    public void getFormWidgets()
+    {
+        txtDate=(TextView) findViewById(R.id.txtdate);
+        txtTime=(TextView) findViewById(R.id.txttime);
+        txtTimeEnd=(TextView)findViewById(R.id.txttime_end);
+        editCv=(EditText) findViewById(R.id.editcongviec);
+        editNd=(EditText) findViewById(R.id.editnoidung);
+        btnDate=(Button) findViewById(R.id.btndate);
+        btnTime=(Button) findViewById(R.id.btntime);
+        btnAdd=(Button) findViewById(R.id.btncongviec);
+        btnTimeEnd=(Button)findViewById(R.id.btntime_end);
+    }
+    /**
+     * Hàm lấy các thông số mặc định khi lần đầu tiền chạy ứng dụng
+     */
+    public void getDefaultInfor()
+    {
+        //lấy ngày hiện tại của hệ thống
+        cal=Calendar.getInstance();
+        SimpleDateFormat dft=null;
+        //Định dạng ngày / tháng /năm
+        dft=new SimpleDateFormat("dd/MM/yyyy",Locale.getDefault());
+        String strDate=dft.format(cal.getTime());
+        //hiển thị lên giao diện
+        txtDate.setText(strDate);
+        //Định dạng giờ phút am/pm
+        dft=new SimpleDateFormat("hh:mm a",Locale.getDefault());
+        String strTime=dft.format(cal.getTime());
+        //đưa lên giao diện
+        txtTime.setText(strTime);
+        //lấy giờ theo 24 để lập trình theo Tag
+        dft=new SimpleDateFormat("HH:mm",Locale.getDefault());
+        txtTime.setTag(dft.format(cal.getTime()));
+
+        String strTimeEnd=dft.format(cal.getTime());
+        //đưa lên giao diện
+        txtTimeEnd.setText(strTimeEnd);
+        //lấy giờ theo 24 để lập trình theo Tag
+        dft=new SimpleDateFormat("HH:mm",Locale.getDefault());
+        txtTimeEnd.setTag(dft.format(cal.getTime()));
+        editCv.requestFocus();
+        //gán cal.getTime() cho ngày hoàn thành và giờ hoàn thành
+        dateFinish=cal.getTime();
+        hourFinish=cal.getTime();
+    }
+    /**
+     * Hàm gán các sự kiện cho các control
+     */
+    public void addEventFormWidgets()
+    {
+        btnDate.setOnClickListener(new MyButtonEvent());
+        btnTime.setOnClickListener(new MyButtonEvent());
+        btnAdd.setOnClickListener(new MyButtonEvent());
+        btnTimeEnd.setOnClickListener(new MyButtonEvent());
+    }
+    /**
+     * Class sự kiện của các Button
+     * @author drthanh
+     *
+     */
+    private class MyButtonEvent implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId())
+            {
+                case R.id.btndate:
+                    showDatePickerDialog();
+                    break;
+                case R.id.btntime:
+                    showTimePickerDialog();
+                    break;
+                case R.id.btntime_end:
+                    showTimePickerDialog1();
+                    break;
+            }
+        }
+
+    }
+    /**
+     * Hàm hiển thị DatePicker dialog
+     */
+    public void showDatePickerDialog()
+    {
+        DatePickerDialog.OnDateSetListener callback=new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year,
+                                  int monthOfYear,
+                                  int dayOfMonth) {
+                //Mỗi lần thay đổi ngày tháng năm thì cập nhật lại TextView Date
+                txtDate.setText(
+                        (dayOfMonth) +"/"+(monthOfYear+1)+"/"+year);
+                //Lưu vết lại biến ngày hoàn thành
+                cal.set(year, monthOfYear, dayOfMonth);
+                dateFinish=cal.getTime();
+            }
+        };
+        //các lệnh dưới này xử lý ngày giờ trong DatePickerDialog
+        //sẽ giống với trên TextView khi mở nó lên
+        String s=txtDate.getText()+"";
+        String strArrtmp[]=s.split("/");
+        int ngay=Integer.parseInt(strArrtmp[0]);
+        int thang=Integer.parseInt(strArrtmp[1])-1;
+        int nam=Integer.parseInt(strArrtmp[2]);
+        DatePickerDialog pic=new DatePickerDialog(
+                MainActivity.this,
+                callback, nam, thang, ngay);
+        pic.setTitle("Chọn ngày hoàn thành");
+        pic.show();
+    }
+    //    Time_End
+    public void showTimePickerDialog1()
+    {
+        TimePickerDialog.OnTimeSetListener callback=new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view,
+                                  int hourOfDay, int minute) {
+                //Xử lý lưu giờ và AM,PM
+                String s=hourOfDay +":"+minute;
+                int hourTam=hourOfDay;
+                if(hourTam>12)
+                    hourTam=hourTam-12;
+                txtTimeEnd.setText
+                        (hourTam +":"+minute +(hourOfDay>12?" PM":" AM"));
+                //lưu giờ thực vào tag
+                txtTimeEnd.setTag(s);
+                //lưu vết lại giờ vào hourFinish
+                cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                cal.set(Calendar.MINUTE, minute);
+                hourFinish=cal.getTime();
+            }
+        };
+
+        //các lệnh dưới này xử lý ngày giờ trong TimePickerDialog
+        //sẽ giống với trên TextView khi mở nó lên
+        String s=txtTimeEnd.getTag()+"";
+        String strArr[]=s.split(":");
+        int gio=Integer.parseInt(strArr[0]);
+        int phut=Integer.parseInt(strArr[1]);
+        TimePickerDialog time=new TimePickerDialog(
+                MainActivity.this,
+                callback, gio, phut, true);
+        time.show();
+    }
+    /**
+     * Hàm hiển thị TimePickerDialog
+     */
+    public void showTimePickerDialog()
+    {
+        TimePickerDialog.OnTimeSetListener callback=new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view,
+                                  int hourOfDay, int minute) {
+                //Xử lý lưu giờ và AM,PM
+                String s=hourOfDay +":"+minute;
+                int hourTam=hourOfDay;
+                if(hourTam>12)
+                    hourTam=hourTam-12;
+                txtTime.setText
+                        (hourTam +":"+minute +(hourOfDay>12?" PM":" AM"));
+                //lưu giờ thực vào tag
+                txtTime.setTag(s);
+                //lưu vết lại giờ vào hourFinish
+                cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                cal.set(Calendar.MINUTE, minute);
+                hourFinish=cal.getTime();
+            }
+        };
+
+        //các lệnh dưới này xử lý ngày giờ trong TimePickerDialog
+        //sẽ giống với trên TextView khi mở nó lên
+        String s=txtTime.getTag()+"";
+        String strArr[]=s.split(":");
+        int gio=Integer.parseInt(strArr[0]);
+        int phut=Integer.parseInt(strArr[1]);
+        TimePickerDialog time=new TimePickerDialog(
+                MainActivity.this,
+                callback, gio, phut, true);
+        time.show();
+    }
+    /**
+     * Hàm xử lý đưa công việc vào ListView khi nhấn nút Thêm Công việc
+     */
+
+
+
+
+
 }
